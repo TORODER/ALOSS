235e55228d2383939385ccf0958150e4801ae7b1
Your Name <you@example.com>
2021-11-29 08:48:41 +0800

    fix shell

--- shell/startEnv.sh
+++ shell/startEnv.sh
@@ -1,3 +1,7 @@
+mkdir -p log
+
+mkdir -p database/data 
+
 nohup mongod --config ./database/mongod.conf > ./log/mongod.nohup.log & 
 
 nohup redis-server ./redis/redis.conf > ./log/redis.nohup.log &
