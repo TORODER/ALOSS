0d1871f7b61350b4d54686772ac67320334a9ba0
autop <1119134719@qq.com>
2021-11-29 15:59:50 +0800

    edit main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { runServerConfig, runServerTestOptions } from "./config/static.config";
 
 (async () => {
     // from static.config  读取内容来启动服务器
-    // 这样设计是为了方便测试(test)
+    // 这样设计是为了方便测试 (UnitTest)
     await bootstrap(runServerConfig, runServerTestOptions);
 })();
